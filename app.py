d419e7362e47820672dc929a16d2efe449c379c7
Mayank Bajaj <41484766+mayank8200@users.noreply.github.com>
2020-09-01 14:25:21 +0530

    commit

--- app.py
+++ app.py
@@ -42,14 +42,14 @@ def upload():
 
         from keras.models import load_model
         new_model = load_model('model.h5')
-        new_model_cat = load_model('MsubCatmodel.h5')
+        new_model_cat = load_model('mysubnewmodel.h5')
 
         new_model.summary()
         test_image = image.load_img('images1\\'+filename,target_size=(60,80))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
         result = new_model.predict(test_image)
-        result1 = new_model.predict(test_image)
+        result1 = new_model_cat.predict(test_image)
         ans = np.argmax(result)
         ans2 = np.argmax(result1)
         ans = key_list[val_list.index(ans)]
